773ad94f5fa32f0d81a3235e0a6d4fe5a7f10f86
LegendaryEmp <jmbabo@byupathway.edu>
2026-05-10 14:35:49 +0100

    week 1 assignments

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Net.Security;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+       
+       Console.WriteLine("What is your grade percentage? ");
+         string gradePercentage = Console.ReadLine();
+         int grade = int.Parse(gradePercentage);
+
+        string letterGrade = "F";
+
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+        }
+
+        Console.WriteLine($"Your letter grade is {letterGrade}.");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass this time, but keep working hard — you'll get it next time!");
+        }
     }
 }
