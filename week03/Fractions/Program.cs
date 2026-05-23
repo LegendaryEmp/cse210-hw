4757565359572ef6fb4967421afec02b4ff43c54
LegendaryEmp <jmbabo@byupathway.edu>
2026-05-23 17:08:07 +0100

    week 3 fraction

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction first = new Fraction();
+        Fraction second = new Fraction(6);
+        Fraction third = new Fraction(6, 7);
+
+        Console.WriteLine(first.GetFractionString());
+        Console.WriteLine(second.GetFractionString());
+        Console.WriteLine(third.GetFractionString());
+
+        second.SetTop(5);
+        second.SetBottom(1);
+
+        Console.WriteLine(second.GetTop());
+        Console.WriteLine(second.GetBottom());
+
+        Fraction one = new Fraction();
+        Fraction five = new Fraction(5);
+        Fraction threeFourths = new Fraction(3, 4);
+        Fraction oneThird = new Fraction(1, 3);
+
+        Console.WriteLine(one.GetFractionString());
+        Console.WriteLine(one.GetDecimalValue());
+        Console.WriteLine(five.GetFractionString());
+        Console.WriteLine(five.GetDecimalValue());
+        Console.WriteLine(threeFourths.GetFractionString());
+        Console.WriteLine(threeFourths.GetDecimalValue());
+        Console.WriteLine(oneThird.GetFractionString());
+        Console.WriteLine(oneThird.GetDecimalValue());
     }
 }
